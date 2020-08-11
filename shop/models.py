7bfc199db656885ac21447b61247577ea8050d99
Rabin Nepal <rabeen1008@gmail.com>
2020-08-11 17:38:47 +0545

    for another machine to clone

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    productId = models.AutoField
+    productName = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subCategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=300)
+    publishedDate = models.DateField()
+    image = models.ImageField(upload_to="shop/images", default="")
+
+    def __str__(self):
+        return self.productName
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Shop")
+    products = Product.objects.all()
+    print(products)
+    allProducts = []
+    catProds = Product.objects.values("category", "id")
+    cats = {item["category"] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProducts.append([prod, range(1, nSlides), nSlides])
+    # params = {"noOfSlide": nSlides, "range": range(1, nSlides), "product": products}
+    # allProducts = [
+    #     [products, range(1, nSlides), nSlides],
+    #     [products, range(1, nSlides), nSlides],
+    # ]
+    params = {"allProducts": allProducts}
+    return render(request, "shop/index.html", params)
+
+
+def about(request):
+    return render(request, "shop/about.html")
+
+
+def contact(request):
+    return HttpResponse("We are in contact section")
+
+
+def tracker(request):
+    return HttpResponse("We are in tracker section")
+
+
+def search(request):
+    return HttpResponse("We are in search section")
+
+
+def productview(request):
+    return HttpResponse("We are in productview section")
+
+
+def checkout(request):
+    return HttpResponse("We are in checkout section")
